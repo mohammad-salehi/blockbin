d1f8670076ff1870a24f71f31d86f626b4cdc499
mohammad <mohammad7979salehi@gmail.com>
2025-10-18 18:01:28 +0330

    entity page start

--- src/app/panel/entity/[id]/page.js
+++ src/app/panel/entity/[id]/page.js
@@ -1,6 +1,23 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect } from 'react'
+import { useParams, usePathname, useRouter } from 'next/navigation'
+import { GetRequest } from '@/functions/GetRequest'
+import { serverAddress } from '@/functions/ServerAddress'
 
 const Page = () => {
+  const params = useParams()
+  const id = params.id
+
+  useEffect(() => {
+    GetRequest(`${serverAddress}/entity/${id}/`)
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  },[])
   return (
     <div>
       id
