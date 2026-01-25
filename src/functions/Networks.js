2a87d8ce5285cd231a8dcda8bf86a32467e95171
mohammad <mohammad7979salehi@gmail.com>
2026-01-25 17:09:40 +0330

    Pin headlessui and use React 18

--- src/functions/Networks.js
+++ src/functions/Networks.js
@@ -5,7 +5,7 @@ export const Networks = [
     { symbole2 : 'DOGE', symbole: 'DOGE', name: 'دوج‌کوین', color: '#dcc46c', id: 15, type: 'utxo' },
     { symbole2 : 'MATIC', symbole: 'MATIC', name: 'متیک', color: '#9f5cdc', id: 7, type: 'account' },
     { symbole2 : 'XRP', symbole: 'XRP', name: 'ریپل', color: '#000000', id: 10, type: 'account' },
-    // { symbole2 : 'TON', symbole: 'TON', name: 'تون', color: '#0098ea', id: 100, type: 'account' },
+    { symbole2 : 'TON', symbole: 'TON', name: 'تون', color: '#0098ea', id: 100, type: 'account' },
     { symbole2 : 'SOL', symbole: 'SOL', name: 'سولانا', color: '#7b80d6', id: 101, type: 'account' },
     { symbole2 : 'BNB', symbole: 'BSC', name: 'بایننس‌اسمارت‌چین', color: '#f3ba2f', id: 5, type: 'account' },
     { symbole2 : 'ETH', symbole: 'ETH', name: 'اتریوم', color: '#627eea', id: 4, type: 'account' },
